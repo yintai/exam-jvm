0df92d35fe396ed8f1c34a4eaaca9b0395ea4604
BrianMa <514972100@qq.com>
2015-06-23 22:51:33 +0800

    correct charset issue in file

--- src/main/java/com/yintai/trx/util/TransactionLookupValues.java
+++ src/main/java/com/yintai/trx/util/TransactionLookupValues.java
@@ -1,17 +1,17 @@
 package com.yintai.trx.util;
 public class TransactionLookupValues {
     
-	//交易状态  成功:1 失败:2 等待付款3
+	//success:1  failed:2  pending:3
 	public static final int TRANSACTION_STATUS_SUCCESS = 1;
     public static final int TRANSACTION_STATUS_FAILED = 2;
     public static final int TRANSACTION_STATUS_PENDING = 3;
     
-    //交易类型  任何类型:-1 支付:1 退款:2
+    //all status:-1 payment:1 refund:2
     public static final int TRANSACTION_TYPE_ALL = -1;
     public static final int TRANSACTION_TYPE_PAYMENT = 1;
     public static final int TRANSACTION_TYPE_REFUND = 2;
     
-    //货币类型  美元:1 人民币:2
+    //dollar:1 rmb:2
     public static final int CURRENCY_TYPE_DOLLAR = 1;
     public static final int CURRENCY_TYPE_RMB = 2;
 }
