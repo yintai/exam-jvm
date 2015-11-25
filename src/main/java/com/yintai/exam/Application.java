8171a12d541b0c008e2f7530ccc55addc590e8c3
tfgzs <867805932@qq.com>
2015-11-26 00:08:14 +0800

    注册h2数据库web管理页面的bean 可以通过 http://localhost:8080/exam/console 来管理嵌入的数据库

--- src/main/java/com/yintai/exam/Application.java
+++ src/main/java/com/yintai/exam/Application.java
@@ -1,7 +1,10 @@
 package com.yintai.exam;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +12,15 @@ public class Application {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class,args);
 	}
-
+	/**
+	 * 加上下面这个方法之后你就可以通过在浏览器里面输入
+	 * http://localhost:8080/exam/console
+	 * 来管理h2数据库,是不是很方便丫 O(∩_∩)O~
+	 */
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+	    ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+	    registration.addUrlMappings("/console/*");
+	    return registration;
+	}
 }
